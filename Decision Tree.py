df5dd03602db1fb35b0629c7bc6fa5c48f373fc0
João Pedro Aguiar <jpaguiar1998@gmail.com>
2022-02-02 19:23:40 +0000

    Add files via upload

--- Decision Tree.py
+++ Decision Tree.py
@@ -1,65 +1,34 @@
-"""
-Imports necessários
-"""
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 import graphviz
 
-#nome das colunas da tabela
-col_names = ['id', 'partner', 'age', 'age_o', 'goal', 'date', 'go_out', 'int_corr', 'length', 'met', 'like', 'prob', 'match']
+# Nome das colunas da tabela
+cn = ['id', 'partner', 'age', 'age_o', 'goal', 'date', 'go_out', 'int_corr', 'length', 'met', 'like', 'prob', 'match']
 
-#nome das classe = nome das colunas
-cn = col_names
+dataset = pd.read_csv("speedDating_trab.csv", header=0, names=cn)
 
-#nome das features (sem o id, o partner, o match e o int_corr)
-fn = ['age', 'age_o', 'goal','int_corr' ,'date', 'go_out', 'length', 'met', 'like', 'prob']
-
-#leitura do dataset
-dataset = pd.read_csv("speedDating_trab.csv", header=0, names=col_names)
-
-#pre-processamento
-#primeiro descartar todas as linhas com ocorrências de NA
+# Pre-processamento
+# Remover linhas com ocorrências de NA e atributos desnecessários (id e partner)
 dataset = dataset.dropna()
-
-#depois descartar os atributos desnecessários
 dataset = dataset.drop('id', axis=1)
 dataset = dataset.drop('partner', axis=1)
 
-#definir o tamanho do conjunto de treino (80% do dataset original)
+# Tamanho do conjunto de treino (80% do dataset original)
 train_length = int(len(dataset) * 0.8)
 
-"""
-Dividir o dataset em conjuntos de treino e teste com a percentagem acima definida
-"""
-train_features = dataset.iloc[:train_length,:-1]
-test_features = dataset.iloc[train_length:,:-1]
-train_targets = dataset.iloc[:train_length,-1]
-test_targets = dataset.iloc[train_length:,-1]
+# Dividir o dataset em treino e teste
+train_features = dataset.iloc[:train_length, :-1]
+test_features = dataset.iloc[train_length:, :-1]
+train_targets = dataset.iloc[:train_length, -1]
+test_targets = dataset.iloc[train_length:, -1]
 
-
-"""
-Treinar o modelo
-"""
-clf = DecisionTreeClassifier(criterion = 'entropy').fit(train_features,train_targets)
-
-
-"""
-Prever o match com base nas restantes classes
-"""
+# Treinar o modelo
+clf = DecisionTreeClassifier(criterion = 'entropy').fit(train_features, train_targets)
+# Prever o match
 prediction = clf.predict(test_features)
-
-
-"""
-Verificar a precisão
-"""
 text_representation = tree.export_text(clf)
 
 print(text_representation)
 print()
-print("Precisão da previsão: ",clf.score(test_features,test_targets)*100,"%")
-
-"""
-Gerar o ficheiro que após compilado gera a imagem da árvore
-"""
-dot_data = tree.export_graphviz(clf, out_file="tree.dot",  feature_names=fn, class_names=cn, filled=True)
+print("Precisão da previsão: ", clf.score(test_features,test_targets)*100, "%")
--- Naive Bayes.py
+++ Naive Bayes.py
@@ -1,47 +1,31 @@
-"""
-Imports necessários
-"""
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-#nome das colunas da tabela
-col_names = ['id', 'partner', 'age', 'age_o', 'goal', 'date', 'go_out', 'int_corr', 'length', 'met', 'like', 'prob', 'match']
+# Nome das colunas da tabela
+cn = ['id', 'partner', 'age', 'age_o', 'goal', 'date', 'go_out', 'int_corr', 'length', 'met', 'like', 'prob', 'match']
 
-#leitura do dataset
-dataset = pd.read_csv("speedDating_trab.csv", header=0, names=col_names)
+dataset = pd.read_csv("speedDating_trab.csv", header=0, names=cn)
 
-#pre-processamento
-#primeiro descartar todas as linhas com ocorrências de NA
+# Pre-processamento
+# Remover linhas com ocorrências de NA e atributos desnecessários (id e partner)
 dataset = dataset.dropna()
-
-#depois descartar os atributos desnecessários
 dataset = dataset.drop('id', axis=1)
 dataset = dataset.drop('partner', axis=1)
 
-#definir o tamanho do conjunto de treino (80% do dataset original)
+# Tamanho do conjunto de treino (80% do dataset original)
 train_length = int(len(dataset) * 0.8)
 
-"""
-Dividir o dataset em conjuntos de treino e teste com a percentagem acima definida
-"""
+# Dividir o dataset em treino e teste
 y = dataset['match']
 X = dataset.drop('match', axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, test_size=0.3)
 
-"""
-Treinar o modelo
-"""
+# Treinar o modelo
 gnb = GaussianNB().fit(X_train, y_train)
+# Prever o match
+prediction = gnb.predict(X_test)
 
-"""
-Fazer previsão dos resultados
-"""
-pred_y = gnb.predict(X_test)
-
-"""
-Verificar a precisão da previsão
-"""
-print("Precisão da previsão do Gaussian Naive Bayes: ", accuracy_score(y_test, pred_y)*100, "%")
+print("Precisão da previsão do Gaussian Naive Bayes: ", accuracy_score(y_test, prediction)*100, "%")
